deec910bc9274d4bc806a541b090dc7bf3962fcf
hisshihi <hissoffc@gmail.com>
2025-02-10 20:57:04 +0500

    Add test for ListAccounts database query with pagination

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -78,3 +78,22 @@ func TestDeleteAccount(t *testing.T) {
 	require.EqualError(t, err, sql.ErrNoRows.Error())
 	require.Empty(t, account2)
 }
+
+func TestListAccounts(t *testing.T) {
+	for range 10 {
+		createRandomAccount(t)
+	}
+
+	arg := ListAccountsParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListAccounts(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
