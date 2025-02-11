94e4bfe8601a5bd583f0a4d63001159b091a769a
hisshihi <hissoffc@gmail.com>
2025-02-11 19:44:27 +0500

    Add comprehensive CRUD tests for entry operations

---
+++ db/sqlc/enty_test.go
@@ -0,0 +1,97 @@
+package sqlc
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/hisshihi/simple-bank-go/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	arg := CreateEntryParams{
+		AccountID: sql.NullInt64{
+			Int64: createRandomAccount(t).ID,
+			Valid: true,
+		},
+		Amount: util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, entry.AccountID)
+	require.Equal(t, arg.Amount, entry.Amount)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	for range 10 {
+		createRandomEntry(t)
+	}
+
+	arg := ListEntriesParams{
+		Limit: 5,
+		Offset: 5,
+	}
+
+	accounts, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, accounts, 5)
+
+	for _, account := range accounts {
+		require.NotEmpty(t, account)
+	}
+}
+
+func TestUpdateEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	
+	arg := UpdateEntryParams{
+		ID: entry1.ID,
+		Amount: util.RandomMoney(),
+	}
+
+	entry2, err := testQueries.UpdateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, arg.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
+
