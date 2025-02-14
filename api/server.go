750f41f378e4b94b4935a7dac8cefe37410e5733
hisshihi <hissoffc@gmail.com>
2025-02-14 19:43:04 +0500

    Add Gin web framework and implement basic account creation API
    Configure project dependencies for Gin, create server and account API endpoints, and update main.go to start the HTTP server with database connection

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hisshihi/simple-bank-go/db/sqlc"
+)
+
+// Server обрабатывает все HTTP-запросы к банковскому сервису
+type Server struct {
+	store *sqlc.Store
+	router *gin.Engine
+}
+
+// NewServer создаёт новый HTTP-сервер и настраивает маршруты
+func NewServer(store *sqlc.Store) *Server {
+	server := &Server{store: store} // создаём сервер
+	router := gin.Default() // создаём маршрутизатор
+
+	router.POST("/accounts", server.createAccount)
+
+	server.router = router // присваиваем маршрутизатор серверу
+	return server // возвращаем сервер
+}
+
+// Запускает HTTP сервер по указанному адресу
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/hisshihi/simple-bank-go/api"
+	"github.com/hisshihi/simple-bank-go/db/sqlc"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
 func main() {
-	
+	// Подключение к базе данных
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := sqlc.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
