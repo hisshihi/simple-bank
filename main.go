b2100151f55c5ce7d9b261b4111a481717ed8ee6
hisshihi <hissoffc@gmail.com>
2025-03-09 22:48:12 +0500

    Добавить конфигурацию приложения с использованием Viper
    Внедрены изменения для централизованного управления конфигурацией:
    - Создан новый файл config.go с использованием библиотеки Viper
    - Добавлен app.env файл для хранения переменных окружения
    - Обновлены main.go и main_test.go для загрузки конфигурации
    - Интегрирована динамическая загрузка параметров подключения к базе данных и серверу

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"database/sql"
 	"log"
 
-	_ "github.com/lib/pq"
 	"github.com/hisshihi/simple-bank/api"
 	"github.com/hisshihi/simple-bank/db/sqlc"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
+	"github.com/hisshihi/simple-bank/util"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := sqlc.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
