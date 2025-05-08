4e000ec1c293902d4c88d240c3a06bb9144f1cf6
hisshihi <hissoffc@gmail.com>
2025-05-08 18:45:15 +0500

    Добавить вывод документации api с помощью swagger

--- cmd/main.go
+++ cmd/main.go
@@ -95,6 +95,9 @@ func runGetwayServer(config config.Config, store sqlc.Store) {
 	mux := http.NewServeMux()
 	mux.Handle("/", grpcMux)
 
+	fs := http.FileServer(http.Dir("doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
 	listener, err := net.Listen("tcp", config.HTTPServerAddress)
 	if err != nil {
 		log.Fatal("cannot create listener", err)
